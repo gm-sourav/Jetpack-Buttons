5b3ba38db197567c2fb3342d11b83d46f64474c3
gm-sourav <souravmahbub13@gmail.com>
2026-04-20 02:02:01 +0600

    initial commit

---
+++ app/src/main/java/com/example/jetpack_buttons/TextButton.kt
@@ -0,0 +1,29 @@
+package com.example.jetpack_buttons
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun TextButtonExmp () {
+    Column(
+        modifier = Modifier.fillMaxSize().padding(horizontal = 16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        TextButton(
+            onClick = { }
+        ) {
+            Text(text = "Forget Password")
+        }
+    }
+}
